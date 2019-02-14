4b40bdcf59ddd8f82300db9dc23cc8aff79124b1
test <test@abc.com>
2019-02-14 15:07:55 +0700

    Authentication Success

---
+++ app/src/main/java/com/suganya/mwavegroup/MyConstant.java
@@ -0,0 +1,15 @@
+package com.suganya.mwavegroup;
+
+public class MyConstant {
+
+    private String urlAddUser = "http://androidthai.in.th/helu/addUser.php";
+    private String urlGetUser = "http://androidthai.in.th/helu/getUserWhereUserPuiiz.php";
+
+    public String getUrlGetUser() {
+        return urlGetUser;
+    }
+
+    public String getUrlAddUser() {
+        return urlAddUser;
+    }
+}
